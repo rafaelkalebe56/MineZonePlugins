89f7185bbf5174dffd9cacaad1d8f715831ff478
rafaelkalebe56 <rafaelkalebe56@gmail.com>
2026-08-20 22:21:46 -0300

    feat: adiciona votação de eventos e kits únicos

--- DisplaySystem/src/main/java/com/minezone/display/interaction/DisplayAction.java
+++
@@ -1,8 +0,0 @@
-package com.minezone.display.interaction;
-
-public record DisplayAction(DisplayActionType type, String value) {
-    public DisplayAction {
-        if (type == null) type = DisplayActionType.NONE;
-        if (value == null) value = "";
-    }
-}
--- DisplaySystem/src/main/java/com/minezone/display/interaction/DisplayActionHandler.java
+++
@@ -1,8 +0,0 @@
-package com.minezone.display.interaction;
-
-import org.bukkit.entity.Player;
-
-@FunctionalInterface
-public interface DisplayActionHandler {
-    void execute(Player player, String value);
-}
